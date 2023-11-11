b11f0240c860cec40e705b3908dafa99226004f9
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-11 16:55:57 -0300

    agrego ej3, dudas

--- js/ej3.js
+++ js/ej3.js
@@ -1,8 +1,29 @@
-let lanzarDado1;
-let lanzarDado2;
-
-function lanzarDado1() {
-  return Math.floor(Math.random() * 7);
+function lanzarElDado() {
+  return Math.floor(Math.random() * 7); //6)+1
 }
 
-document.write(`<p class="formatoTexto">${lanzarDado1}</p>`);
+function simularLanzarDado() {
+  const apariciones = Array(11).fill(0);
+
+  let lanzamientos = 0;
+
+  while (lanzamientos < 50) {
+    const suma = lanzarElDado() + lanzarElDado();
+    apariciones[suma - 2]++;
+    lanzamientos++;
+  }
+
+  document.write(`<h2 class="formatoTitulos">Resultados:</h2>`);
+  document.write(`<table class="formatoTexto">`);
+  document.write(`<tr><th>Suma 🎲🎲</th><th>Apariciones ✨</th></tr>`);
+
+  for (let i = 2; i <= 12; i++) {
+    document.write(
+      "<tr><td>" + i + "</td><td>" + apariciones[i - 2] + "</td></tr>"
+    );
+  }
+
+  document.write("</table>");
+}
+
+simularLanzarDado();
